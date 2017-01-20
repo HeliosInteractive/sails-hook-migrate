dc6a488760f5ed5d38a65a9e03132674990dadfd
Michael Neil <michael.neil@heliosinteractive.com>
2017-01-20 15:07:33 -0800

    Added usage info and using actual settings from sails

---
+++ README.md
@@ -0,0 +1,36 @@
+# Sails Hook Migrate
+
+Uses [DB Migrate](https://github.com/db-migrate/node-db-migrate) to handle database migrations.
+
+Supported Databases:
+ - MySQL
+ - PostgreSQL
+ - sqlite3
+ - MongoDB
+
+## Dependencies (optional)
+
+**Highly recommended** to use with `sails-generate-migrate` to create migration scripts from the command line.
+
+```sh
+npm i -S sails-generate-migrate
+``
+
+## Installation
+
+``sh
+npm i -S sails-generate-migrate sails-hook-migrate
+``
+
+## Usage
+
+Create a migrations folder and run sails lift!
+
+ - Create a `migrations` folder
+ - `sails lift`
+
+Migrate will detect the connection specified by your sails config. You can also override the connection settings
+in the configuration.
+
+## Configuration
+
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+'use strict';
+
+const DBMigrate = require('db-migrate');
+
 module.exports = function(sails) {
+
+  var adapters = [
+    {driver: 'sqlite3', regex: /sqlite/i},
+    {driver: 'mysql', regex: /mysql/i},
+    {driver: 'pg', regex: /postgre/i},
+    {driver: 'mongodb', regex: /mongo/i},
+  ]
+
   return {
     defaults: {
       __configKey__:{
@@ -10,7 +22,39 @@ module.exports = function(sails) {
     },
     initialize: function(done){
 
-      return done();
+      var connection;
+      try{
+        connection = sails.config.connections[sails.config.models.connection];
+      }catch(e){}
+
+      if( !connection || !connection.adapter){
+        sails.log.warn('Connection not supported or missing adapter');
+        return done();
+      }
+
+      let driver = adapters.filter(adapter => adapter.regex.test(connection.adapter))
+        .reduce((o, n) => n.driver || o, false);
+
+      if( !driver ){
+        sails.log.warn('adapter %s not supported for sails-hook-migrate', connection.adapter);
+        return done();
+      }
+
+      var migrate = DBMigrate.getInstance(true, {
+          config: {
+            [process.env.NODE_ENV]: Object.assign({
+              driver: driver,
+            }, connection)
+          }
+        });
+
+      migrate.up(function(){
+        //console.log('done', arguments.length);
+        //console.log('arguments', Array.prototype.slice.call(arguments))
+        return done();
+      });
+
+
     }
   };
 }
